d3ea5f1a21e87e4fdcd6206c27a6b3a9a647788e
Hector Ruiz <ruizhernandezhector57@gmail.com>
2024-06-16 00:09:44 -0600

    conexion creada

--- php/sesion.php
+++ php/sesion.php
@@ -1,12 +1,22 @@
 <?php
-    include("conexion.php");
-    $user=$_POST['user'];
-    $pass=$_POST['pass'];
-    
-    $buscar="SELECT * FROM usuarios WHERE user='$user' AND pass='$pass'";
-    $q=mysqli_query($con, $buscar);//Ejecutamos la consulta
-    if(mysqli_num_rows($q)==0){
-        echo "Usuario no encontrado";
-        Header("Location: ../sesion.html?error=1");
-    }
+session_start();
+
+include("conexion.php");
+
+$user = $_POST['user'];
+$pass = $_POST['pass'];
+
+$buscar = "SELECT * FROM clientes WHERE USUARIO='$user' AND password='$pass'";
+$q = mysqli_query($con, $buscar);
+
+if(mysqli_num_rows($q) > 0){
+    // Inicio de sesión exitoso, redirigir a la página principal
+    $_SESSION['user'] = $user; // Guardar el nombre de usuario en sesión si es necesario
+    header("Location: ../index.html");
+    exit();
+} else {
+    // Credenciales incorrectas, redirigir de vuelta a sesion.html con un parámetro de error
+    header("Location: ../sesion.html?error=1");
+    exit();
+}
 ?>
