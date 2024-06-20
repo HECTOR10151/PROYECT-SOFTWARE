8768f9d1e1dede2a19f5a53dc9883e02c24f8c13
MarcerLM <mario02monroy@gmail.com>
2024-06-19 23:58:13 -0600

    Editar Citar (prueba)

--- php/editarcitas.php
+++ php/editarcitas.php
@@ -59,33 +59,54 @@ $rowCliente = mysqli_fetch_array($qCliente);
         </div>
     </nav>
 
-    <h1>Editar Cita</h1>
-    <form action="updatecitas.php" method="POST">
-        <input type="hidden" name="id" value="<?php echo $rowCita['IDcitas']; ?>">
+    <div class="banner">
+        <br>
+        <h1>Editar cita</h1>
+    </div>
 
-        <label for="usuario">Usuario: <?php echo $rowCliente['USUARIO']; ?></label><br>
+    <div class="formularioNuevoUsuario">
+        <form action="updatecitas.php" method="POST">
+            <input type="hidden" name="id" value="<?php echo $rowCita['IDcitas']; ?>">
+            <div class="form-group">
+                <label for="usuario"> Cita del usuario: <?php echo $rowCliente['USUARIO']; ?></label>
+            </div>
 
-        <label for="direccion">Dirección:</label>
-        <input type="text" id="direccion" name="direccion" value="<?php echo $rowCita['DIRECCION']; ?>"><br><br>
+            <div class="form-grop">
+                <label for="direccion">Dirección:</label>
+                <input class="form-control"  type="text" id="direccion" name="direccion" value="<?php echo $rowCita['DIRECCION']; ?>">
+            </div>
 
-        <label for="fecha">Fecha:</label>
-        <input type="date" id="fecha" name="fecha" value="<?php echo $rowCita['FECHA']; ?>"><br><br>
+            <div class="form-row">
+                <div class="col">
+                    <label for="material">Material:</label>
+                    <input  class="form-control" type="text" id="material" name="material" value="<?php echo $rowCita['MATERIAL']; ?>">
+                </div>
+                <div class="col">
+                    <label for="kilos">Kilos:</label>
+                    <input class="form-control"  type="number" id="kilos" name="kilos" value="<?php echo $rowCita['KILOS']; ?>">
+                </div>
+            </div>
 
-        <label for="material">Material:</label>
-        <input type="text" id="material" name="material" value="<?php echo $rowCita['MATERIAL']; ?>"><br><br>
+            <div class="form-row">
+                <div class="col">
+                    <label for="estatus">Estatus:</label>
+                    <select id="estatus" name="estatus">
+                        <option value="PENDIENTE" <?php if ($rowCita['ESTATUS'] == 'PENDIENTE') echo 'selected'; ?>>Pendiente</option>
+                        <option value="COMPLETADO" <?php if ($rowCita['ESTATUS'] == 'COMPLETADO') echo 'selected'; ?>>Completado</option>
+                    </select>
+                </div>
+                <div class="col">
+                    <label for="fecha">Fecha:</label>
+                    <input class="form-control"  type="date" id="fecha" name="fecha" value="<?php echo $rowCita['FECHA']; ?>">
+                </div>
+            </div>
+            <div class="form-group">    
+                <button type="submit">Actualizar</button>
+                <button type="button"  class="btn btn-primary" id="botonInicio" onclick="redireccion()">Cancelar</button>
+            </div>
+        </form>
+    </div>
 
-        <label for="kilos">Kilos:</label>
-        <input type="number" id="kilos" name="kilos" value="<?php echo $rowCita['KILOS']; ?>"><br><br>
-
-        <label for="estatus">Estatus:</label>
-        <select id="estatus" name="estatus">
-            <option value="PENDIENTE" <?php if ($rowCita['ESTATUS'] == 'PENDIENTE') echo 'selected'; ?>>Pendiente</option>
-            <option value="COMPLETADO" <?php if ($rowCita['ESTATUS'] == 'COMPLETADO') echo 'selected'; ?>>Completado</option>
-        </select><br><br>
-
-        <button type="submit">Actualizar</button>
-        <button type="button" onclick="redireccion()">Cancelar</button>
-    </form>
     <script>
         function redireccion() {
             location.href = "admin.php";
@@ -108,12 +129,14 @@ $rowCliente = mysqli_fetch_array($qCliente);
                 <i class="bx bxl-tiktok social_icon"></i>
             </a>
             <br>
-            <p class="mb-0">RopaTech - Transformando moda en sostenibilidad. © 2024 Todos los derechos reservados. 
-                <b><h6><a class="social_link tam" href="#">Aviso de privacidad</a> | <a class="social_link tam" href="#">Términos de Servicio</a> | <a class="social_link tam" href="#">Contáctanos</a></h6></b>
+            <p class="mb-0">RopaTech - Transformando moda en sostenibilidad. © 2024 Todos los derechos reservados.
+                <b>
+                    <h6><a class="social_link tam" href="#">Aviso de privacidad</a> | <a class="social_link tam" href="#">Términos de Servicio</a> | <a class="social_link tam" href="#">Contáctanos</a></h6>
+                </b>
             </p>
         </div>
     </footer>
-    
+
 </body>
 
 </html>
